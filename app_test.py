47a62127b27258a48e68af730ddf7bf58106bcd6
Kaique da Silva <kaique.silva.dev@gmail.com>
2018-04-20 13:52:16 -0300

    Well defined test cases

---
+++ app_test.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+
+class StatisticsStructAboutGameTestCase(tf.test.TestCase):
+	pass
+
+class TimeStepsTestCase(tf.test.TestCase):
+	pass
+
+class EnvironmentLoggingTestCase(tf.test.TestCase):
+	pass
+
+class IterationLoggingTestCase(tf.test.TestCase):
+	pass
+
+class RenderingTestCase(tf.test.TestCase):
+	pass
+
+class StepsTestCase(tf.test.TestCase):
+	pass
+
+class CollectiblesTestCase(tf.test.TestCase):
+	pass
+
+class ProgressTestCase(tf.test.TestCase):
+	pass
