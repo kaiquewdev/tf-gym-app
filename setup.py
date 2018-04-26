daaafeabe949e5a44f170c2c79e9635d7148caf5
Kaique da Silva <kaique.silva.dev@gmail.com>
2018-04-26 16:13:09 -0300

    Setup file for a specific content

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='qlearning',
+      version='1.0.0',
+      description='Q-learning algorithms based on app structure',
+      author='Kaique da Silva',
+      author_email='kaique.silva.dev@gmail.com',
+      url='git@github.com:kaiquewdev/tf-gym-app',
+      download_url='https://github.com/kaiquewdev/tf-gym-app/tarball/1.0.0',
+      license='BSD',
+      install_requires=['numpy>=1.14.2',
+                        'tensorflow>=1.7.0',
+                        'Keras>=2.1.4',
+                        'pandas>=0.22.0',
+                        'gym>=0.10.5'],
+      extras_require={
+      	'h5py': ['h5py']
+      },
+	  classifiers=[
+	  	'Development Status :: 5 - Production/Stable',
+	  	'Intended Audience :: Developers',
+	  	'Intended Audience :: Education',
+	  	'Intended Audience :: Science/Research',
+	  	'License :: OSI Approved :: BSD License',
+	  	'Programming Language :: Python :: 3.6',
+	  	'Topic :: Software Development :: Libraries',
+	  	'Topic :: Software Development :: Libraries :: Python Modules'
+	  ],
+	  packages=find_packages())
